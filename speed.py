0b598de26683437d4f51b320eda84f115a251c6f
liangkai <59242240+nanyanglk@users.noreply.github.com>
2021-05-31 10:30:09 +0800

    Update speed.py
    注释掉脚本内不需要的行，并添加说明

--- speed.py
+++ speed.py
@@ -2,12 +2,12 @@ import time
 def bar():
     scale = 50
     print("\033[32m打开进程中...\033[0m".center(65, " "))
-#    start = time.perf_counter()
+#    start = time.perf_counter() 开始时间
     for i in range(scale+1):
         a = '*' * i
         b = '.' * (scale - i)
         c = i/scale*100
-#        dur = time.perf_counter() - start
-        print("\r[{}->{}]{:^3.0f}%".format(a,b,c),end="") 
+#        dur = time.perf_counter() - start  开始时间减去现在时间 = 已用时间
+        print("\r[{}->{}]{:^3.0f}%".format(a,b,c),end="")  #去掉dur变量引用
 
         time.sleep(0.01)
