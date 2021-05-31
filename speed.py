d3f0b3a6a82d3aa637c8da27470fc5970de61dc9
liangkai <59242240+nanyanglk@users.noreply.github.com>
2021-05-31 10:28:48 +0800

    Update speed.py
    注释掉脚本内不需要的行

--- speed.py
+++ speed.py
@@ -2,12 +2,12 @@ import time
 def bar():
     scale = 50
     print("\033[32m打开进程中...\033[0m".center(65, " "))
-    start = time.perf_counter()
+#    start = time.perf_counter()
     for i in range(scale+1):
         a = '*' * i
         b = '.' * (scale - i)
         c = i/scale*100
-        dur = time.perf_counter() - start
-        print("\r[{}->{}]{:^3.0f}%".format(a,b,c,dur),end="") 
+#        dur = time.perf_counter() - start
+        print("\r[{}->{}]{:^3.0f}%".format(a,b,c),end="") 
 
         time.sleep(0.01)
